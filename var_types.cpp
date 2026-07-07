512716f9fe106645ccc1ab7db8fcc1fc9c8f2c59
Kajal1925 <kajalkumari2622021@gmail.com>
2026-07-07 14:05:24 +0530

    Add files via upload

---
+++ var_types.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age=20;
+    float height=5.4;
+    char grade='A';
+    bool isstu= true;
+
+    cout<<"Age : "<<age<<endl;
+    cout<<"Height : "<<height<<endl;
+    cout<<"Grade : "<<grade<<endl;
+    cout<<"Is Student : "<<isstu<<endl;
+
+    return 0;
+}
