821aca64a951c993f56107c0781d26b32f422ed0
Kajal1925 <kajalkumari2622021@gmail.com>
2026-07-07 14:20:20 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1, num2;
+
+    cout<<"Enter number1: "<<endl;
+    cin>>num1;
+    cout<<"Enter number2: "<<endl;
+    cin>>num2;
+    
+    cout<<"Sum is: "<<num1+num2;
+
+    return 0;
+}
