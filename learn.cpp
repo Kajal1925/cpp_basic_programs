46654ae137d0ae9262fcbc18404985514aeadf2c
Kajal1925 <kajalkumari2622021@gmail.com>
2026-07-07 12:47:17 +0530

    Delete learn.cpp

--- learn.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream> // Includes the library for input and output operations
-using namespace std; 
-
-int main(){
-    cout<<"Hello World!.";  
-    
-    return 0; // Signifies that the program executed successfully
-}
