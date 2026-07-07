207459e177bc04870043b1785edf22b42334ac10
Kajal1925 <kajalkumari2622021@gmail.com>
2026-07-07 12:53:22 +0530

    Add files via upload

---
+++ info.cpp
@@ -0,0 +1,11 @@
+#include<iostream> // Includes the library for input and output operations
+using namespace std; 
+
+int main(){
+    cout<<"Name : Kajal"<<endl;
+    cout<<"Age : 20"<<endl;
+    cout<<"City : Dehradun"<<endl;
+    
+    
+    return 0; // Signifies that the program executed successfully
+}
