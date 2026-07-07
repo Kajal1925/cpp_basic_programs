7d1f4f8a5e1dae0689d558e73b44b834612b42ef
Kajal1925 <kajalkumari2622021@gmail.com>
2026-07-07 14:37:16 +0530

    Add files via upload

---
+++ arithmetic.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float num1,num2;
+
+    cout<<"Enter number1: "<<endl;
+    cin>>num1;
+    cout<<"Enter number2: "<<endl;
+    cin>>num2;
+
+    cout<<"Addition: "<<num1+num2<<endl;
+    cout<<"Subtraction: "<<num1-num2<<endl;
+    cout<<"Multiplication: "<<num1*num2<<endl;
+    cout<<"Division: "<<num1/num2<<endl;
+    return 0;
+}
