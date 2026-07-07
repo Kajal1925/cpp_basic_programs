78933c4540890e501cf083269a89a98376e022ff
Kajal1925 <kajalkumari2622021@gmail.com>
2026-07-07 12:46:29 +0530

    Add files via upload

---
+++ learn.cpp
@@ -0,0 +1,8 @@
+#include<iostream> // Includes the library for input and output operations
+using namespace std; 
+
+int main(){
+    cout<<"Hello World!.";  
+    
+    return 0; // Signifies that the program executed successfully
+}
