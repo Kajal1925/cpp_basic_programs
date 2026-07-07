9c3913818da298926971461068efdf6c7d1db63a
Kajal1925 <kajalkumari2622021@gmail.com>
2026-07-07 15:06:00 +0530

    Add files via upload

---
+++ sq_and_cube.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"Enter number : "<<endl;
+    cin>>num;
+    cout<<"Square : "<<num*num<<endl;
+    cout<<"Cube : "<<num*num*num<<endl;
+    return 0;
+}
