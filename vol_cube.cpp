bd1811923bc5acf58eb2844bddf4f836c8f7b840
Kajal1925 <kajalkumari2622021@gmail.com>
2026-07-09 18:47:52 +0530

    Add files via upload

---
+++ vol_cube.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float l,b,h,vol;
+    cout<<"Volume of Cube"<<endl;
+    cout<<"Enter the length of cube: "<<endl;
+    cin>>l;
+    cout<<"Enter the breadth of cube: "<<endl;
+    cin>>b;
+    cout<<"Enter the height of cube: "<<endl;
+    cin>>h;
+    vol=l*b*h;
+    cout<<"Volume of the cube is: "<<vol;cout<<"m cube."<<endl;
+    return 0;
+
+}
