0ac97a21df48a1dd2e537453a2d80035c63f40cc
Kajal1925 <kajalkumari2622021@gmail.com>
2026-07-09 17:25:13 +0530

    Add files via upload

---
+++ circle_ar.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float rad,area;
+    cout<<"Enter the radius of  circle: "<<endl;
+    cin>>rad;
+    area=3.14*rad*rad;
+    cout<<"Area of the circle is : "<<area;cout<<" m sq."<<endl;
+    return 0;
+}
