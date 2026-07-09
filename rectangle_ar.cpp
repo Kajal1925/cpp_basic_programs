5e5b7e1e13c419bea6a186e02c82fda8411cef39
Kajal1925 <kajalkumari2622021@gmail.com>
2026-07-09 17:13:12 +0530

    Add files via upload

---
+++ rectangle_ar.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int l,b,area;
+    cout<<"Enter the length of rectangle: "<<endl;
+    cin>>l;
+    cout<<"Enter the breadth of rectangle: "<<endl;
+    cin>>b;
+    area=l*b;
+    cout<<"The area of rectangle is : "<<area;cout<<" m sq."<<endl;
+    return 0;
+    
+}
