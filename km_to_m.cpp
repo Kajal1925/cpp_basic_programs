17e33491701a42d31d404fba52bc21987e9dd16e
Kajal1925 <kajalkumari2622021@gmail.com>
2026-07-09 17:55:22 +0530

    Add files via upload

---
+++ km_to_m.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float km,m;
+    cout<<"Kilometer into Meter"<<endl;
+    cout<<"Enter the distance (in km) : "<<endl;
+    cin>>km;
+    m=km*1000;
+    cout<<"The distance in meters is : "<<m;cout<<" m"<<endl;
+    return 0;
+    
+
+}
