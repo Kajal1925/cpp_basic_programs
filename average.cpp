a57764e37a3f2722c9c5a1dd748bc3606ba06188
Kajal1925 <kajalkumari2622021@gmail.com>
2026-07-09 17:47:25 +0530

    Add files via upload

---
+++ average.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float a,b,c,avg;
+    cout<<"Enter a: "<<endl;
+    cin>>a;
+    cout<<"Enter b: "<<endl;
+    cin>>b;
+    cout<<"Enter c: "<<endl;
+    cin>>c;
+    avg=(a+b+c)/3;
+    cout<<"The average od the three numbers is : "<<avg<<endl;
+    return 0;
+}
