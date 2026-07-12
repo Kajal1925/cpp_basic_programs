2cea320dae4e150207e8f4dd6f1b37326e72ab84
Kajal1925 <kajalkumari2622021@gmail.com>
2026-07-12 16:09:12 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"To print factorial of a number"<<endl;
+    int n,factorial=1;
+    cout<<"Enter a number: ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        factorial=factorial*i;
+    }
+    cout<<"Factorial is: "<<factorial;
+    return 0;
+}
