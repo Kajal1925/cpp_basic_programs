a46e91bc863b09f4c95e86b71cef46b011df70f0
Kajal1925 <kajalkumari2622021@gmail.com>
2026-07-22 00:05:39 +0530

    Add files via upload

---
+++ greeting.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+void greeting(){
+    cout<<"Hello World :)"<<endl;
+}
+int main(){
+    greeting();
+    return 0;
+}
--- sum.cpp
+++ sum.cpp
@@ -1,15 +1,17 @@
 #include<iostream>
 using namespace std;
 
+int sum(int num1, int num2);
+
 int main(){
-    int num1, num2;
-
-    cout<<"Enter number1: "<<endl;
+    int num1,num2;
+    cout<<"Enter first number: ";
     cin>>num1;
-    cout<<"Enter number2: "<<endl;
+    cout<<"Enter second number: ";
     cin>>num2;
-    
-    cout<<"Sum is: "<<num1+num2;
-
+    cout<<"Sum of "<<num1<<" and "<<num2<<" is: "<<sum(num1, num2);
     return 0;
+}
+int sum(int num1, int num2){
+    return num1+num2;
 }
