4b3e1309a667e8deccad3ea5ab2e8c47c53988b4
Kajal1925 <kajalkumari2622021@gmail.com>
2026-07-23 23:23:04 +0530

    Add files via upload

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,28 +1,29 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int n;
-    int t1=0;
-    int t2=1;
-    int next_t=0;
-    cout<<"Enter the number of terms: ";
-    cin>>n;
-    cout<<"Fibonacci Series: "<<endl;
-    for(int i=1; i<=n; i++){
-        if(i==1){
-            cout<<t1<<" "<<endl;
-            continue;
-        }
-        if(i==2){
-            cout<<t2<<" "<<endl;
-            continue;
-        }
-        next_t=t1+t2;
-        t1=t2;
-        t2=next_t;
-        cout<<next_t<<" "<<endl;
+void printFibonacciSeries(int n) {
+    if (n <= 0) {
+        cout << "Please enter a positive integer." <<endl;
+        return;
+    }
+
+    int first = 0, second = 1;
+
+    cout << "Fibonacci Series: ";
+    for (int i = 0; i < n; ++i) {
+        cout << first << " ";
+        
+        int next = first + second;
+        first = second;
+        second = next;
     }
     cout<<endl;
+}
+
+int main() {
+    int terms;
+    cout<<"Enter number of terms: ";
+    cin>> terms;
+    printFibonacciSeries(terms);
     return 0;
 }
